49134e88fb81a848293d124a076943a49f66ce68
It's me <74585420+cmp9797@users.noreply.github.com>
2023-06-02 04:13:38 +0700

    Update GameViewController.swift

--- SignPal/SignTalk/Presenters/Views/MiniGameView/GameViewController.swift
+++ SignPal/SignTalk/Presenters/Views/MiniGameView/GameViewController.swift
@@ -13,7 +13,7 @@ class GameViewController: UIViewController {
     let signPoseClassifier = SignPoseClassifier()
     let miniGameManager = MiniGameManager()
     
-    var detectedPose = ""
+    var detectedPose = SignPose(classificationName: "")
     var timer: Timer?
 
     @IBOutlet weak var questionLbl: UILabel!
@@ -32,6 +32,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLbl.text = miniGameManager.question
+        detectedPose = signPoseClassifier.signPosePrediction
     }
     
 }
@@ -41,25 +42,16 @@ extension GameViewController {
     // MARK: Main storyboard updates
     /// Updates the storyboard's image view.
 
-    /// Notifies the view controller when a user selects a photo in the camera picker or photo library picker.
-    /// - Parameter photo: A photo from the camera or photo library.
-    func validatePhoto(_ photo: UIImage) {
-//        DispatchQueue.global(qos: .userInitiated).async {
-            self.classifyTheSign(photo)
-
-//        }
-        showPopup()
-    }
-    
     func showPopup() {
         print("quest: \(miniGameManager.question) // label: \(questionLbl.text ?? "nayy")")
         print("ans: \(detectedPose)")
         
-        if miniGameManager.isCorrect(detectedPose: detectedPose) {
+        if miniGameManager.isCorrect(detectedPose: detectedPose.classificationName) {
             popUpIconLbl!.image = UIImage(systemName: "checkmark.circle.fill")
             popUpIconLbl.tintColor = UIColor.green
             
             popUpTextLbl.text = "Yeay, you got it!"
+            generateNewQuestion()
             
         } else {
             popUpIconLbl.image = UIImage(systemName: "xmark.circle.fill")
@@ -70,24 +62,17 @@ extension GameViewController {
         
         self.popUpIconLbl.backgroundColor = UIColor(named: "White1")
         
-//        view.addSubview(popUpIconLbl)
-//        view.addSubview(popUpTextLbl)
-        
         popUpIconLbl.isHidden = false
         popUpTextLbl.isHidden = false
         
         // Start the timer to dismiss the pop-up after a certain duration
         timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(dismissPopup), userInfo: nil, repeats: false)
 
-        
-//        dismissPopup()
     }
     
     @objc func dismissPopup() {
         popUpIconLbl.isHidden = true
         popUpTextLbl.isHidden = true
-//        popUpIconLbl?.removeFromSuperview()
-//        popUpTextLbl?.removeFromSuperview()
         timer?.invalidate()
     }
     
@@ -96,31 +81,17 @@ extension GameViewController {
         questionLbl.text = miniGameManager.question
     }
     
-}
-
-extension GameViewController {
-    // MARK: Image prediction methods
-    /// Sends a photo to the Image Predictor to get a prediction of its content.
-    /// - Parameter image: A photo.
-    private func classifyTheSign(_ image: UIImage) {
-        do {
-            try self.signPoseClassifier.makePredictions(for: image,
-                                                    completionHandler: imagePredictionHandler)
-        } catch {
-            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
-        }
-    }
-
-    /// The method the Image Predictor calls when its image classifier model generates a prediction.
-    /// - Parameter predictions: An array of predictions.
-    /// - Tag: imagePredictionHandler
-    private func imagePredictionHandler(_ predictions: [SignPoseClassifier.Prediction]?){
-        guard let predictions = predictions else {
-            print("No Prediction")
-            return
-        }
+    /// Notifies the view controller when a user selects a photo in the camera picker or photo library picker.
+    /// - Parameter photo: A photo from the camera or photo library.
+    func validatePhoto(_ photo: UIImage) {
+//        DispatchQueue.global(qos: .userInitiated).async {
+        self.signPoseClassifier.classifyTheSign(photo)
         
-        detectedPose = String(describing: predictions.first!.classification)
+        detectedPose = signPoseClassifier.signPosePrediction
+//        }
+        showPopup()
     }
-
+    
+    
+    
 }
